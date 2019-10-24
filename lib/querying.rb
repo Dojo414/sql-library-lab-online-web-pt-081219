8e4ab2ff120d958b7896a5ca12ae00afc19ea40c
Donovan Jones <donovanljones94@gmail.com>
2019-10-24 17:16:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT (*) FROM characters"
+  "SELECT species, COUNT (*) FROM characters
+   ORDER BY COUNT "
 end
 
 def select_name_and_series_subgenres_of_authors

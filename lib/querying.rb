ff7a351e4564c9180b04033c5ac1985be429abd1
Donovan Jones <donovanljones94@gmail.com>
2019-10-24 17:20:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, series.subgenre FROM authors
+   INNER JOIN series
+   ON sereies.author_id = author.id;"
 end
 
 def select_series_title_with_most_human_characters

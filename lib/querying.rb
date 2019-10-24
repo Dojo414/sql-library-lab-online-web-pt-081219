b5358c2830049a64ac3271b849bdc4d5621956e4
Donovan Jones <donovanljones94@gmail.com>
2019-10-24 17:25:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,10 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    INNER JOIN books 
-   ON series.id = books.series_id"
+   ON series.id = books.series_id
+   INNER JOIN characters 
+   ON characters.book_id = books.id
+   "
 end
 
 def select_character_names_and_number_of_books_they_are_in

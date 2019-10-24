b40058eedc0300316a32e06425d5abdb736bd899
Donovan Jones <donovanljones94@gmail.com>
2019-10-24 17:18:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT (*) FROM characters
-   GROUP BY COUNT (species)
+   GROUP BY (species)
+   ORDER BY COUNT
    DESC LIMIT 1;"
 end
 
